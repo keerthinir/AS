8a61ae9c3a2e921e30a1cc5cbff7bc7a18d45c66
keerthini <keerthini.r@cognitiveclouds.com>
2018-03-01 19:38:08 +0530

    first commit

---
+++ american-steel-inc/views/our-company.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title>American Steel</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="../css/main.css">
+</head>
+<body>
+
+  
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="../js/jquery-3.1.1.slim.min.js"></script>
+  <script src="../js/tether.min.js"></script>
+  <script src="../js/bootstrap.min.js"></script>
+  <script src="../js/custom.js"></script>
+  
+</body>
+</html>
