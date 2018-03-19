a55f3027490be87b768fe390fb1bbf30a5768af8
keerthini <keerthini.r@cognitiveclouds.com>
2018-03-19 18:58:44 +0530

    zipcode n subscribe modal

--- american-steel-inc/js/custom.js
+++ american-steel-inc/js/custom.js
@@ -1,3 +1,21 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+	$('.category-item-list > li > a').mouseover(function() {
+		$(this).tab('show');
+	});
+	$( ".home-banner,.navbar" ).mouseover(function() {
+		$('.category-item-list > li > a,.tab-content .tab-pane ').removeClass('active');
+	});
+
 });
+
+$(window).on('load',function(){
+	$('#zipCodeModal').modal('show');
+});
+
+// subscribeModal
+setInterval(showModal, 60000);
+
+function showModal()
+{
+  $('#subscribeModal').modal('show');
+}
